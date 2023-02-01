7b23ff41212b24e31b003d89d8f9750e7ef23991
kyon-MR <108559388+kyon-MR@users.noreply.github.com>
2023-02-01 17:38:35 +0900

    Create index.php

---
+++ PHP/3-1/index.php
@@ -0,0 +1,20 @@
+<?php 
+
+  for ($sum=1; $sum<100; $sum++) {
+    if ($sum%3 === 0 && $sum%5 === 0) {
+      echo "FizzBuzz!!";
+      echo '<br>';
+    } elseif ($sum%3 === 0) {
+      echo "Fizz!";
+      echo '<br>';
+    } elseif ($sum%5 === 0) {
+      echo "Buzz!";
+      echo '<br>';
+    } else {
+      echo $sum;
+      echo '<br>';
+    }
+  }
+
+
+?>
