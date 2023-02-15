a8c9eb28c759eed18ca551e7ef88b13177fec00c
kyon-MR <108559388+kyon-MR@users.noreply.github.com>
2023-02-15 14:55:54 +0900

    Update index.php

--- 3-2/index.php
+++ 3-2/index.php
@@ -1,10 +1,18 @@
 <?php
 
-$fruits = ["りんご" => 300, "みかん" => 150, "もも" => 3000];
+$fruits = ["りんご" => 100, "みかん" => 50, "もも" => 300];
+$number = [3, 3, 10];
 
+function getPrice($fruits, $number) {
+  $total = $fruits * $number;
+  return $total;
+}
+
+$i = 0;
 foreach ($fruits as $key => $value) {
-  print $key."は".$value."円です。";
-  echo '<br>';
+  print $key."は".getPrice($value, $number[$i])."円です。";
+  print '<br>';
+  $i++;
 }
 
 ?>
