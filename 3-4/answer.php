c28c86e8c98549bbbce7c69bed0d48589c2015b2
kyon-MR <108559388+kyon-MR@users.noreply.github.com>
2023-03-10 00:13:52 +0900

    Add files via upload

---
+++ 3-4/answer.php
@@ -0,0 +1,43 @@
+<link rel="stylesheet" href="styles.css">
+<?php 
+//[question.php]から送られてきた名前の変数、選択した回答、問題の答えの変数を作成
+$name = $_POST['name'];
+$Ans1 = $_POST['Ans1'];
+$Ans2 = $_POST['Ans2'];
+$Ans3 = $_POST['Ans3'];
+$correctAns1 = $_POST['correctAns1'];
+$correctAns2 = $_POST['correctAns2'];
+$correctAns3 = $_POST['correctAns3'];
+//選択した回答と正解が一致していれば「正解！」、一致していなければ「残念・・・」と出力される処理を組んだ関数を作成する
+function checkAnswer1($Ans1, $correctAns1) {
+  if ($Ans1 === $correctAns1) {
+    echo "正解!";
+  } else {
+    echo "残念・・・";
+  }
+}
+function checkAnswer2($Ans2, $correctAns2) {
+  if ($Ans2 === $correctAns2) {
+    echo "正解!";
+  } else {
+    echo "残念・・・";
+  }
+}
+function checkAnswer3($Ans3, $correctAns3) {
+  if ($Ans3 === $correctAns3) {
+    echo "正解!";
+  } else {
+    echo "残念・・・";
+  }
+}
+?>
+<p><!--POST通信で送られてきた名前を表示--><?php echo $name; ?>さんの結果は・・・？</p>
+<p>①の答え</p>
+<!--作成した関数を呼び出して結果を表示-->
+<?php checkAnswer1($Ans1, $correctAns1); ?>
+<p>②の答え</p>
+<!--作成した関数を呼び出して結果を表示-->
+<?php checkAnswer2($Ans2, $correctAns2); ?>
+<p>③の答え</p>
+<!--作成した関数を呼び出して結果を表示-->
+<?php checkAnswer3($Ans3, $correctAns3); ?>
---
+++ 3-4/question.php
@@ -0,0 +1,40 @@
+<link rel="stylesheet" href="styles.css">
+<?php
+//POST送信で送られてきた名前を受け取って変数を作成
+$my_name = $_POST['my_name'];
+//①画像を参考に問題文の選択肢の配列を作成してください。
+$number = [80, 22, 20, 21];
+$langueage = ["PHP", "Python", "JAVA", "HTML"];
+$command = ["join", "select", "insert", "update"]; 
+//② ①で作成した、配列から正解の選択肢の変数を作成してください
+$correctAns1 = 80;
+$correctAns2 = "HTML";
+$correctAns3 = "select";
+
+?>
+<p>お疲れ様です<!--POST通信で送られてきた名前を出力--><?php echo $my_name?>さん</p>
+<!--フォームの作成 通信はPOST通信で-->
+<form action="answer.php" method="post">
+  <h2>①ネットワークのポート番号は何番？</h2>
+  <!--③ 問題のradioボタンを「foreach」を使って作成する-->
+  <?php foreach($number as $value) {?>
+  <input type="radio" name="Ans1"><?php echo $value; ?>
+  <?php } ?>
+<h2>②Webページを作成するための言語は？</h2>
+<!--③ 問題のradioボタンを「foreach」を使って作成する-->
+<?php foreach($langueage as $lgValue) {?>
+  <input type="radio" name="Ans2"><?php echo $lgValue; ?>
+  <?php } ?>
+<h2>③MySQLで情報を取得するためのコマンドは？</h2>
+<!--③ 問題のradioボタンを「foreach」を使って作成する-->
+<?php foreach($command as $cdValue) {?>
+  <input type="radio" name="Ans3"><?php echo $cdValue; ?>
+  <?php } ?>
+<!--問題の正解の変数と名前の変数を[answer.php]に送る-->
+  <input type="hidden" name="name" value="<?php echo $my_name; ?>">
+  <input type="hidden" name="correctAns1" value="<?php echo $correctAns1; ?>">
+  <input type="hidden" name="correctAns2" value="<?php echo $correctAns2; ?>">
+  <input type="hidden" name="correctAns3" value="<?php echo $correctAns3; ?>">
+  <br>
+  <input type="submit" value="回答する">
+  </form>
