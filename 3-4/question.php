72ce83937799595c7f5d9d673adfe6b7d46c4973
kyon-MR <108559388+kyon-MR@users.noreply.github.com>
2023-03-10 14:26:09 +0900

    Add files via upload

--- 3-4/question.php
+++ 3-4/question.php
@@ -18,23 +18,23 @@ $correctAns3 = "select";
   <h2>①ネットワークのポート番号は何番？</h2>
   <!--③ 問題のradioボタンを「foreach」を使って作成する-->
   <?php foreach($number as $value) {?>
-  <input type="radio" name="Ans1"><?php echo $value; ?>
+  <input type="radio" name="Ans1" value="<?php echo $value; ?>"><?php echo $value; ?>
   <?php } ?>
 <h2>②Webページを作成するための言語は？</h2>
 <!--③ 問題のradioボタンを「foreach」を使って作成する-->
 <?php foreach($langueage as $lgValue) {?>
-  <input type="radio" name="Ans2"><?php echo $lgValue; ?>
+  <input type="radio" name="Ans2" value="<?php echo $lgValue; ?>"><?php echo $lgValue; ?>
   <?php } ?>
 <h2>③MySQLで情報を取得するためのコマンドは？</h2>
 <!--③ 問題のradioボタンを「foreach」を使って作成する-->
 <?php foreach($command as $cdValue) {?>
-  <input type="radio" name="Ans3"><?php echo $cdValue; ?>
+  <input type="radio" name="Ans3" value="<?php echo $cdValue; ?>"><?php echo $cdValue; ?>
   <?php } ?>
 <!--問題の正解の変数と名前の変数を[answer.php]に送る-->
-  <input type="hidden" name="name" value="<?php echo $my_name; ?>">
-  <input type="hidden" name="correctAns1" value="<?php echo $correctAns1; ?>">
-  <input type="hidden" name="correctAns2" value="<?php echo $correctAns2; ?>">
-  <input type="hidden" name="correctAns3" value="<?php echo $correctAns3; ?>">
+  <input type="hidden" name="name" value="<?php echo $my_name; ?>"><?php echo $my_name; ?>
+  <input type="hidden" name="correctAns1" value="<?php echo $correctAns1; ?>"><?php echo $correctAns1; ?>
+  <input type="hidden" name="correctAns2" value="<?php echo $correctAns2; ?>"><?php echo $correctAns2; ?>
+  <input type="hidden" name="correctAns3" value="<?php echo $correctAns3; ?>"><?php echo $correctAns3; ?>
   <br>
   <input type="submit" value="回答する">
   </form>
